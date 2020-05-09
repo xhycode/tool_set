63118717f9d68c20c18a0b76754d289e4f1184cd
xhycode <731374786@qq.com>
2020-05-09 19:36:25 +0800

    NEW：波形图的背景色改成白色

--- ui/global_ui.py
+++ ui/global_ui.py
@@ -16,6 +16,7 @@ class GloabalUI(QtWidgets.QMainWindow, Ui_ToolSet):  # 继承类
     g_waveform_signal = QtCore.pyqtSignal(str, list)
     g_waveform_clear_signal = QtCore.pyqtSignal(str)
     set_lcd_recv_len_signal = QtCore.pyqtSignal(bool, int)
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # 执行类中的setupUi函数
@@ -31,7 +32,7 @@ class GloabalUI(QtWidgets.QMainWindow, Ui_ToolSet):  # 继承类
     def renew_recv_dispay(self, data):
         self.e_recv.moveCursor(QtGui.QTextCursor.End)
         self.e_recv.insertPlainText(data)
-    
+
     def renew_serial_port(self, port_list, cur):
         self.serial_port.clear()
         self.serial_port.addItems(port_list)
@@ -52,15 +53,13 @@ class GloabalUI(QtWidgets.QMainWindow, Ui_ToolSet):  # 继承类
         else:
             self.lcd_recv_len.display(num)
 
-    ############# 波形界面相关方法 ##################
+    # ############ 波形界面相关方法 ##################
 
     def init_waveform_ui(self):
+        self.g_waveform.setBackground('w')
         self.g_waveform.addLegend()
         self.g_waveform.showGrid(x=True, y=True, alpha=0.5)
-        self.g_waveform.setLabels(left='y', bottom='x',
-                            title='y/x')  # left纵坐标名 bottom横坐标名
-        label = pyqtgraph.TextItem()
-        self.g_waveform.addItem(label)
+        self.g_waveform.setLabels(left='y', bottom='x',title='y/x')
         self.curves = {}
 
     def set_waveform_data(self, name, data):
@@ -74,7 +73,7 @@ class GloabalUI(QtWidgets.QMainWindow, Ui_ToolSet):  # 继承类
         COLOR = ['b', 'g', 'r', 'c', 'm', 'y', 'w', 'k']
         color = COLOR[len(self.curves) % len(COLOR)]
         self.curves[name] = self.g_waveform.plot(pen=color, name=name)
-    
+
     def clear_curves(self, name):
         if self.curves.get(name, None):
             self.curves[name].clear()

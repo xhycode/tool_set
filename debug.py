6bd062848cda761c4860bcdb965b097715f52c4a
hongyan xie <731374786@qq.com>
2020-05-09 08:20:35 +0800

    ENH: debug 消息添加中断打印

--- debug.py
+++ debug.py
@@ -2,10 +2,12 @@ from ui import ui
 
 
 def info(info):
+    print(info)
     '''将要显示的字符串信息输出到调试窗口'''
     ui.e_debug_info_signal.emit(info)
 
 
 def info_ln(info):
+    print(info+'\n')
     '''将要显示的字符串信息输出到调试窗口'''
     ui.e_debug_info_signal.emit(info+'\n')

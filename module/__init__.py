b6a2c9a084eeded88bf59f0d6f9cefc8aa18fa54
hongyan xie <731374786@qq.com>
2020-05-11 23:36:15 +0800

    NEW: 添加当前界面到配置文件，下次打开保存界面不变

--- module/__init__.py
+++ module/__init__.py
@@ -4,6 +4,7 @@ from module import module_base
 from module import waveform
 from module import data_display
 from ui import ui
+import cfg
 
 class ModuleManage(module_base.ModuleBase):
     def __init__(self):
@@ -15,11 +16,14 @@ class ModuleManage(module_base.ModuleBase):
         self._set_cur_module()
 
     def _set_cur_module(self):
-        self._cur_module = self._module_table[self._mode_win.currentIndex()]
+        last_index = int(cfg.get(cfg.LAST_MODULE_INDEX, '0'))
+        self._mode_win.setCurrentIndex(last_index)
+        self._cur_module = self._module_table[last_index]
 
     def _event_chenge(self):
-        print(self._mode_win.currentIndex())
-        self._set_cur_module()
+        cur_index = self._mode_win.currentIndex()
+        self._cur_module = self._module_table[cur_index]
+        cfg.set(cfg.LAST_MODULE_INDEX, str(cur_index))
 
     def parse(self, data):
         self._cur_module.parse(data)

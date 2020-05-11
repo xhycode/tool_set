15be270cf9441909a2c0df01e297c85061822312
hongyan xie <731374786@qq.com>
2020-05-11 08:27:32 +0800

    NEW:添加波形通道的清除和隐藏功能

--- module/waveform.py
+++ module/waveform.py
@@ -15,89 +15,89 @@ STATU_INDEX = 2
 COLOR = ['b', 'g', 'r', 'c', 'm', 'y', 'w', 'k']
 
 class _curve():
-    def __init__(self, data_cnt=1000):
+    def __init__(self, chn_name, chn_num, cache_size=1000):
         self.data = []
-        self.data_cnt = data_cnt
-        self.is_show = True
+        self.cache_size = cache_size
+        self.chn_name = chn_name
+        self.b_clean = getattr(ui, 'b_chn_clear_' + str(chn_num))
+        self.c_chn = getattr(ui, 'c_chn_' + str(chn_num))
+        self.c_chn.setCheckState(2)
+        self.c_chn.setText(chn_name)
 
-    def clear(self):
+    def clear_data(self):
         self.data = []
+        ui.g_waveform_clear_signal.emit(self.chn_name)
 
     def append(self, data):
-        if len(self.data) >= self.data_cnt:
+        if len(self.data) >= self.cache_size:
             self.data = self.data[1:]
         self.data.append(data)
 
+    def set_cache(self, size):
+        self.cache_size = size
+        if len(self.data) >= self.cache_size:
+            self.data = self.data[self.cache_size - len(self.data):]
+
+    def renew_diaplay(self):
+        if self.c_chn.checkState():
+            ui.g_waveform_signal.emit(self.chn_name, self.data)
+        else:
+            ui.g_waveform_clear_signal.emit(self.chn_name)
+
+
+MAX_CHANNAL_COUNT = 16
 
 class Waveform(QThread, ModuleBase):
     def __init__(self):
         super().__init__()
+        self.channal_count = 0
+        self.channal_cache = 1000
         self.curves = {}
         self.line_data = ''
+        ui.c_chn_all.stateChanged.connect(self.all_show)
+        ui.b_chn_clear_all.clicked.connect(self.all_clear)
         self.start()
 
-    def get_frame(self):
-        '''获取帧，方便嵌入到其他窗口显示'''
-        pass
+    def new_chn_num(self):
+        if self.channal_count >= MAX_CHANNAL_COUNT:
+            return None
+        chn_num = self.channal_count
+        self.channal_count += 1
+        return chn_num
 
     def _get_chn_info(self, chn):
         '''获取波形界面,如果没有就添加一个新通道'''
         if not ui.get_curves(chn):
-            ui.add_curves(chn)
-            self.curves[chn] = _curve()
+            chn_num = self.new_chn_num()
+            if chn_num is not None:
+                ui.add_curves(chn)
+                self.curves[chn] = _curve(chn, chn_num, self.channal_cache)
+                # 放到类中子类中不能触发信号，所以放在这里了
+                self.curves[chn].b_clean.pressed.connect(self.chn_clear_data)
+
+            else:
+                return None
         return self.curves[chn]
 
-    def run(self):
-        while True:
-            for chn, curve in self.curves.items():
-                if curve.is_show:
-                    ui.g_waveform_signal.emit(chn, curve.data)
-                else:
-                    curve.plot.clear()
-            QThread.msleep(20)
-
-    def show(self, chn):
-        '''显示对应的通道数据'''
-        if self.curves.get(chn):
-            self.curves[chn].is_show = True
-
-    def hide(self, chn):
-        '''隐藏对应的通道数据'''
-        if self.curves.get(chn):
-            self.curves[chn].is_show = False
+    def all_show(self):
+        for curve in self.curves.values():
+            curve.c_chn.setCheckState(ui.c_chn_all.checkState())
 
     def append(self, chn, data):
         '''添加一个数据到通道 data是数字'''
         d = self._get_chn_info(chn)
-        d.append(data)
+        if d:
+            d.append(data)
 
-    def set_data(self, chn, data=[]):
-        '''直接赋值数据到对应通道，data是列表'''
-        d = self._get_chn_info(chn)
-        d.data = data
-        ui.g_waveform_signal.emit(chn, d.data)
+    def chn_clear_data(self):
+        for curve in self.curves.values():
+            if curve.b_clean.isDown():
+                curve.clear_data()
+        
 
-    def clear(self, chn):
-        '''清除对应通道的数据'''
-        if self.curves.get(chn):
-            self.curves[chn].clear()
-            ui.g_waveform_clear_signal.emit(chn)
-
-    def clear_all(self):
-        '''清除所有通道的数据'''
-        for chn in self.curves.keys():
-            self.curves[chn].clear()
-
-    def get(self):
-        '''获取通道信息,通道名列表形式 [ch1_name, ch2_name,...]'''
-        return list(self.curves.keys())
-
-    def pop(self, chn):
-        '''获取通道最后一个数据，不会删除'''
-        if self.curves.get(chn):
-            return self.curves[chn].data[-1]
-        else:
-            return None
+    def all_clear(self, chn):
+        for curve in self.curves.values():
+            curve.clear_data()
 
     def _between_str(self, string, start, end):
         head = string.find(start)
@@ -109,12 +109,21 @@ class Waveform(QThread, ModuleBase):
 
     def parse(self, data):
         ch = data.decode()
-        if ch is '\n':
-            debug.info_ln(self.line_data)
-            name = self._between_str(self.line_data, '<', '>')
-            d = self._between_str(self.line_data, '[', ']')
-            if name and d:
-                self.append(name, float(d))
-            self.line_data = ''
+        if ch == '\n':
+            try:
+                debug.info_ln(self.line_data)
+                name = self._between_str(self.line_data, '<', '>')
+                d = self._between_str(self.line_data, '[', ']')
+                if name and d:
+                    self.append(name, float(d))
+                self.line_data = ''
+            except:
+                debug.info_ln('数据解析错误')
         else:
             self.line_data += ch
+
+    def run(self):
+        while True:
+            for curve in self.curves.values():
+                curve.renew_diaplay()
+            QThread.msleep(20)

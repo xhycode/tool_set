29e8b0b1abcf18bcbf9f7ec637708c63c2296ee0
xhycode <731374786@qq.com>
2020-05-15 19:30:27 +0800

    NEW: 控制类分别创建发送和接收线程

--- control.py
+++ control.py
@@ -4,28 +4,44 @@ from message import Message
 from module import ModuleManage
 
 
-class Control(QThread):  # 继承类
+class ControlThread(QThread):
+    def __init__(self, target):
+        super().__init__()
+        self.target = target
+
+    def run(self):
+        self.target()
+
+
+class Control():  # 继承类
     '''
         控制数据与各个模块间的通信
     '''
 
     def __init__(self):
-        super().__init__()
         self.msg = Message()  # 消息处理
         self.md = ModuleManage()  # 功能模块管理
-        self.start()
+        self.recv_thread = ControlThread(self.recv)
+        self.send_thread = ControlThread(self.send)
+        self.recv_thread.start()
+        self.send_thread.start()
 
-    def run(self):
+    def recv(self):
         ''' 说明：线程 start 后调用的函数
             功能：接收数据并赋值到各模块
         '''
         while True:
             if not self.msg.status():
-                self.msleep(100)
+                QThread.msleep(100)
                 continue
             data = self.msg.recv()
             if data is not None:
                 if len(data) > 0:
                     self.md.parse(data)
             else:
-                self.msleep(10)
+                QThread.msleep(10)
+
+    def send(self):
+        while(True):
+            print('send')
+            QThread.msleep(500)

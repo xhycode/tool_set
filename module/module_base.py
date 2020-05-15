91648fd40077af20523809d81b46e5ece1f1aa67
xhycode <731374786@qq.com>
2020-05-15 19:22:22 +0800

    NEW: 模块基类添加发送数据的缓存
    控制类中读取缓存用于发送

--- module/module_base.py
+++ module/module_base.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
+import queue
 
 
 class ModuleBase():
-    def __init__(self):
-        pass
+    def __init__(self, buf_size=0):
+        self.send_queue = queue.Queue(buf_size)
 
     def parse(self, data):
         pass
+
+    def send_push(self, data):
+        if self.send_queue.full():
+            return False
+        else:
+            self.send_queue.put(data)
+            return True
+
+    def send_pop(self):
+        if self.send_queue.empty():
+            return None
+        else:
+            return self.send_queue.get()
+
+
+if __name__ == "__main__":
+    module = ModuleBase()
+    module.send_push("G28\n")
+    module.send_push("G29\n")
+    print(module.send_pop())
+    print(module.send_pop())

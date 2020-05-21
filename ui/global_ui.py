4c793f55bcae67af3889cce21ba3b28ae247f17f
hongyan xie <731374786@qq.com>
2020-05-21 23:38:58 +0800

    ENH: 整理全局显示的代码

--- ui/global_ui.py
+++ ui/global_ui.py
@@ -14,13 +14,24 @@ TOOL_VERSIONS = 'V1.1.1'
 
 class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
     app = QApplication(sys.argv)
+
     '''外部在线程里边调用的小部件,需要提供一个自定义信号供外部使用，不然会出错'''
-    e_recv_signal = QtCore.pyqtSignal(str)
+    # 调试信号窗口输出信号 str:要显示的字符串  bool:True-html数据;False-纯字符串
     e_debug_info_signal = QtCore.pyqtSignal(str, bool)
-    serial_port_signal = QtCore.pyqtSignal(list, str)
-    g_waveform_signal = QtCore.pyqtSignal(str, list)
-    g_waveform_clear_signal = QtCore.pyqtSignal(str)
+
+    # 数据显示窗口添加要显示的内容的信号 str:显示的内容
+    e_recv_signal = QtCore.pyqtSignal(str)
+    # 更新接收长度的信号  bool: True-数字累加  False-直接赋值
     set_lcd_recv_len_signal = QtCore.pyqtSignal(bool, int)
+
+    # 刷新串口列表的信号  list串口列表; str:当前显示的串口
+    serial_port_signal = QtCore.pyqtSignal(list, str)
+
+    # 设置波形数据的信号 str:通道对应的名称 list:数据列表
+    g_waveform_signal = QtCore.pyqtSignal(str, list)
+    # 清空指定通道波形数据显示的信号 str:通道对应名称
+    g_waveform_clear_signal = QtCore.pyqtSignal(str)
+   
 
     def __init__(self):
         super().__init__()
@@ -28,35 +39,65 @@ class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
         self.setWindowTitle("调试工具箱 " + TOOL_VERSIONS)
         self.icon_init()
         self.init_waveform_ui()
+        self.signal_init()
+    
+    def icon_init(self):
+        '''界面坐上角显示的图标'''
+        self.setWindowIcon(QIcon('./ui/mao.png'))
+
+    def signal_init(self):
+        '''将开头定义的信号量绑定处理函数'''
         self.e_recv_signal.connect(self.renew_recv_dispay)
+        self.set_lcd_recv_len_signal.connect(self.set_lcd_recv_len)
         self.e_debug_info_signal.connect(self.debug_dispay)
         self.serial_port_signal.connect(self.renew_serial_port)
         self.g_waveform_clear_signal.connect(self.clear_curves)
         self.g_waveform_signal.connect(self.set_waveform_data)
-        self.set_lcd_recv_len_signal.connect(self.set_lcd_recv_len)
-    
-    def icon_init(self):
-        self.setWindowIcon(QIcon('./ui/mao.png'))
 
     def renew_recv_dispay(self, data):
+        ''' 数据界面添加内容，数据会追加到最后边显示
+            data: 要显示的数据，str类型
+        '''
         self.e_recv.moveCursor(QTextCursor.End)
         self.e_recv.insertPlainText(data)
-
-    def highlight_selection(self, text_edit, format):
-        cursor = text_edit.textCursor()
-        if not cursor.hasSelection():
-            cursor.select(QTextCursor.WordUnderCursor)
-        cursor.mergeCharFormat(format)
-        text_edit.mergeCurrentCharFormat(format)
     
+    def set_lcd_recv_len(self, is_add, num):
+        ''' 设置接收数据长度的显示
+        is_add: True-数字累加 类型: bool
+        num: 更新的数字  类型:int
+        '''
+        if is_add:
+            self.lcd_recv_len.display(self.lcd_recv_len.value() + num)
+        else:
+            self.lcd_recv_len.display(num)
+
     def text_all_black(self, text_edit):
+        ''' 将所有的字体都变成黑色
+            text_edit: 要做修改的部件 类型: QTextEdit
+        '''
         col = QColor('black')
         fmt = QTextCharFormat()
         fmt.setForeground(col)
         text_edit.selectAll()
         text_edit.setCurrentCharFormat(fmt)
 
+    def _highlight_selection(self, text_edit, format):
+        ''' 高亮选中的符串
+            text_edit: 要做修改的部件 类型: QTextEdit
+            format : 指定的字符串格式 类型: QTextCharFormat
+        '''
+        cursor = text_edit.textCursor()
+        if not cursor.hasSelection():
+            cursor.select(QTextCursor.WordUnderCursor)
+        cursor.mergeCharFormat(format)
+        text_edit.mergeCurrentCharFormat(format)
+    
     def highlight_text(self, text_edit, text, color):
+        ''' QTextEdit 中的部分字体高亮
+            text_edit: 待操作的小部件 
+            text: 要高亮的字符串
+            color：字符串颜色
+        '''
         self.text_all_black(text_edit)
         col = QColor(color)
         fmt = QTextCharFormat()
@@ -65,12 +106,17 @@ class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
             return
         if not col.isValid():
             return
-        # 先把光标移动到开头
+        # 先把光标移动到开头, 然后全文查找
         text_edit.moveCursor(QTextCursor.Start)
         while text_edit.find(text):  # 查找所有文字
-            self.highlight_selection(text_edit, fmt)
+            self._highlight_selection(text_edit, fmt)
 
     def renew_serial_port(self, port_list, cur):
+        ''' 刷新串口列表
+            port_list: 串口列表  类型: list
+            cur : 当前要显示的端口 类型:str
+            如果列表中没有 cur 端口则显示列表第一个
+        '''
         self.serial_port.clear()
         self.serial_port.addItems(port_list)
         print(port_list)
@@ -80,6 +126,11 @@ class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
             self.serial_port.setCurrentText(port_list[0])
 
     def debug_dispay(self, data, is_html):
+        ''' 调试窗口输出信息
+            data: 要显示的信息 类型: str
+            is_html: True-按照html显示  False-纯字符显示  类型: bool
+            由于没有清空的操作，所以会限制显示的量
+        '''
         text = self.e_debug_info.toPlainText()
         if len(text) > 4096:  # 大小现在在4k，超过就砍半
             self.e_debug_info.clear()
@@ -90,33 +141,43 @@ class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
         else:
             self.e_debug_info.insertPlainText(data)
 
-    def set_lcd_recv_len(self, flag, num):
-        if flag:
-            self.lcd_recv_len.display(self.lcd_recv_len.value() + num)
-        else:
-            self.lcd_recv_len.display(num)
-
     # ############ 波形界面相关方法 ##################
 
     def init_waveform_ui(self):
+        ''' 波形显示界面初始化
+        '''
         self.g_waveform.setBackground('w')
         self.g_waveform.addLegend()
         self.g_waveform.showGrid(x=True, y=True, alpha=0.5)
         self.g_waveform.setLabels(left='y', bottom='x',title='y/x')
-        self.curves = {}
+        self.curves = {}  # 存放 通道名:曲线 组成的字典
 
     def set_waveform_data(self, name, data):
+        ''' 将数据设置到指定的通道显示
+            name: 通道名称 类型: str
+            data: 要显示的数据 类型: list
+        '''
         if self.curves.get(name, None):
             self.curves[name].setData(data)
 
     def get_curves(self, name):
+        ''' 获取指定通道的曲线
+            name: 通道名字 类型: str
+            返回: 曲线类 找不到返回 None
+        '''
         return self.curves.get(name, None)
 
     def add_curves(self, name):
+        ''' 添加一个曲线显示通道
+            name:通道名 类型: str
+        '''
         COLOR = ['b', 'g', 'r', 'c', 'm', 'y', 'w', 'k']
         color = COLOR[len(self.curves) % len(COLOR)]
         self.curves[name] = self.g_waveform.plot(pen=color, name=name)
 
     def clear_curves(self, name):
+        ''' 清除一个通道的显示
+            name:通道的名字 类型:str
+        '''
         if self.curves.get(name, None):
             self.curves[name].clear()

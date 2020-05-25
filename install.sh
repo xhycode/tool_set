958505549bda5a7e97688824c678361630e5c37b
xhycode <731374786@qq.com>
2020-05-26 07:45:32 +0800

    NEW: 上传linux环境安装脚本

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip
+sudo pip3 install serial -i https://pypi.douban.com/simple
+sudo pip3 install pyserial -i https://pypi.douban.com/simple
+sudo pip3 install configobj -i https://pypi.douban.com/simple
+sudo pip3 install pyqtgraph -i https://pypi.douban.com/simple
+sudo apt-get install python3-pyqt5
+alias pyuic5="python3 -m PyQt5.uic.pyuic"

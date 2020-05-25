ee9673a826b708a0225956d77da782c6bf186841
xhycode <731374786@qq.com>
2020-05-26 07:41:27 +0800

    NEW: 整理message.__init__.py

--- message/__init__.py
+++ message/__init__.py
@@ -19,9 +19,11 @@ AUTO_SEND_EXTEND = '2'
 
 
 class Message(QThread):
+    ''' 管理不同的消息类型,对外开放统一的接口'''
+
     def __init__(self):
         super().__init__()
-        self.connect_init()
+        self.message_module_init()
         self.send_init()
         self.extend_send_init()
         self.bind()
@@ -41,15 +43,19 @@ class Message(QThread):
         ui.e_auto_send_time.valueChanged.connect(self.auto_send_time_save)
         ui.c_hex_send.stateChanged.connect(self.hex_send_state_save)
 
-    def connect_init(self):
+    def message_module_init(self):
+        ''' 初始化已有的连接方式 '''
         self.serial = serial_tool.SerialMesssge()
         self.client = tcp_client.TCPClinet()
         self.server = tcp_server.TCPServer()
+        # 当前连接设置成上次使用的连接方式，默认是串口连接
         self.connect_mode = cfg.get(cfg.MSG_CONNRET_MODE, CONNTET_SERIAL)
         self.set_connect_mode(self.connect_mode)
         self.state = False  # 连接状态
 
     def set_connect_mode(self, connect):
+        ''' 将当前连接设置到选择的连接方式，并保存到配置文件
+        '''
         self.connect_mode = connect
         if connect == CONNTET_SERIAL:
             self.cur_connect = self.serial
@@ -63,15 +69,19 @@ class Message(QThread):
         cfg.set(cfg.MSG_CONNRET_MODE, self.connect_mode)
 
     def extend_send_init(self):
-        self.extend_send_index = 0
-        self.extend_count = 22
-        self.extend_send_info = []
+        ''' 扩展(快捷)发送区的初始化
+            扩展区的所有数据都会保存，初始化会恢复上次的数据
+        '''
+        self.extend_send_index = 0  # 顺序发送时用到的索引记录
+        self.extend_count = 22  # 扩展发送区发送栏的个数
+        self.extend_send_info = []  # 列表的每个成员是个发送栏
         for i in range(self.extend_count):
             temp = {}
-            temp['btn'] = getattr(ui, 'b_extend_send_' + str(i + 1))
-            temp['data'] = getattr(ui, 'e_extend_send_' + str(i + 1))
-            temp['select'] = getattr(ui, 'c_extend_send_' + str(i + 1))
-            last_data = cfg.get('extend_data_' + str(i+1))
+            # getattr 从类中根据属性的名字字符串获取属性，由于名字有规律，所以用循环方便
+            temp['btn'] = getattr(ui, 'b_extend_send_' + str(i + 1))  # 发送的按键
+            temp['data'] = getattr(ui, 'e_extend_send_' + str(i + 1))  # 数据栏
+            temp['select'] = getattr(ui, 'c_extend_send_' + str(i + 1))  # 选择框
+            last_data = cfg.get('extend_data_' + str(i+1))  # 恢复上次关闭时的数据
             temp['data'].setText(last_data)
             last_select = cfg.get('extend_selsct_' + str(i+1), '0')
             temp['select'].setCheckState(int(last_select))
@@ -83,16 +93,21 @@ class Message(QThread):
         ui.c_extend_cyclic_send.setCheckState(int(cfg.get(cfg.EXTEND_CYCLIC, '0')))
 
     def send_init(self):
+        ''' 底部发送区部件的初始化及发送线程初始化
+        '''
         self.send_queue = queue.Queue()  # 存放bytes类型数据
         self.auto_send_mode = AUTO_SEND_NONE
         self.auto_send = QTimer()
         self.auto_send.timeout.connect(self._event_auto_send_timer)
         ui.c_hex_send.setCheckState(int(cfg.get(cfg.HEX_SEND_STATE, '0')))
-        ui.e_auto_send_time.setValue(float(cfg.get(cfg.AUTO_SELD_TIME, '1.0')))
+        ui.e_auto_send_time.setValue(float(cfg.get(cfg.AUTO_SEND_TIME, '1.0')))
         self.send_encode_init()
-        self.start()  # 开启发送线程
+        self.start()  # 开启发送线程，继承来的，从 run() 函数运行
 
     def send_encode_init(self):
+        ''' 发送可以使用不同的编码格式，
+            要正确的选择编码，要不接收端可能解码不了
+        '''
         self.cur_encode = cfg.get(cfg.SEND_ENCODE, 'GB2312')
         table = ['UTF-8', 'GB2312', 'ASCLL', 'ANSI', 'GBK', 'UNICODE', 'GB18030']
         ui.c_send_encode.addItems(table)
@@ -100,53 +115,79 @@ class Message(QThread):
         ui.c_send_encode.currentTextChanged.connect(self.change_encode)
 
     def change_encode(self, encode):
+        ''' 发送编码更改的事件处理
+        '''
         self.cur_encode = encode
         cfg.set(cfg.SEND_ENCODE, encode)
         debug.info('发送编码：' + encode)
 
     def extend_enter_status_save(self, state):
+        ''' 保存扩展发送的换行复选框状态 
+            复选框改变事件调用
+        '''
         cfg.set(cfg.EXTEND_ENTER_STATE, state)
 
     def extend_cyclic_status_save(self, state):
+        ''' 保存扩展发送的循环发送复选框状态 
+            复选框改变事件调用
+        '''
         cfg.set(cfg.EXTEND_CYCLIC, state)
 
     def extend_send_save(self):
+        ''' 扩增发送区的数据有变化就全部保存一遍，
+            人的输入还是很慢的，所以全都保存没什么影响
+        '''
         for i in range(self.extend_count):
-            cfg.set('extend_data_' + str(i+1),
+            cfg.set('extend_data_' + str(i+1),  # 输入数据的保存
                     self.extend_send_info[i]['data'].displayText())
-            cfg.set('extend_selsct_' + str(i+1),
+            cfg.set('extend_selsct_' + str(i+1),  # 选中复选框的状态保存
                     self.extend_send_info[i]['select'].checkState())
 
     def hex_send_state_save(self, state):
+        ''' 保存发送区的十六进制发送复选框状态 
+            复选框改变事件调用
+        '''
         cfg.set(cfg.HEX_SEND_STATE, str(state))
     
     def auto_send_time_save(self, new_time):
-        cfg.set(cfg.AUTO_SELD_TIME, '{:.3f}'.format(new_time))
+        ''' 保存发送区的十六进制发送复选框状态 
+            复选框改变事件调用
+        '''
+        cfg.set(cfg.AUTO_SEND_TIME, '{:.3f}'.format(new_time))
 
     def get_next_extend_data(self):
+        ''' 循环发送时获取下一个要发送的数据
+            没被选中的不发送
+        '''
         index = self.extend_send_index
         while True:
             if self.extend_send_info[index]['select'].checkState():
+                # 找到下一个被选中的数据
                 data = self.extend_send_info[index]['data'].displayText()
                 if ui.c_entend_enter.checkState():
-                    data += '\n'
+                    data += '\n'  # 勾选了自动换行
                 self.extend_send_index = index + 1
                 return data
-            index = (index + 1) % self.extend_count
-            if index == self.extend_send_index:
+            index = (index + 1) % self.extend_count  # 循环累加，到最大会从头开始
+            if index == self.extend_send_index:  # 找了一圈没有选中的
                 debug.err('没有选中的数据')
                 return None
-            if index == 0:
-                if not ui.c_extend_cyclic_send.checkState():
+            if index == 0:  # 循环一圈了，要判断是否重新循环
+                if not ui.c_extend_cyclic_send.checkState():  # 没勾选循环选项
                     debug.info('顺序发送结束')
                     return None
 
     def recv_line(self):
+        ''' 收到 \n 才会停止接收
+        '''
         if self.cur_connect.status():
             return self.cur_connect.recv_line()
         return None
 
     def recv(self, count=1):
+        ''' 接收 count 数据，默认是1个
+            连接异常返回 None
+        '''
         try:
             if self.cur_connect.status():
                 return self.cur_connect.recv(count)
@@ -154,14 +195,19 @@ class Message(QThread):
         except:
             if not self.state:
                 return None  # 主动断开不重新打开
+            # self.state 没被置位说明意外断开了连接，尝试重新打开一次
             if not self.cur_connect.status():
                 self.cur_connect.event_open()
+            # 再判断一次打开是否成功
             if not self.cur_connect.status():
                 ui.b_status_control.setText('连接断开')
                 debug.err('连接被断开')
             return None
 
     def send(self, data, encode='GB2312', ishex=False):
+        ''' data ：类型 str
+            这个发送不是立即发送，而是放到缓存区等待线程发送
+        '''
         try:
             if ishex:
                 b_data = bytes.fromhex(data)
@@ -180,7 +226,11 @@ class Message(QThread):
         return self.cur_connect.status()
 
     def _event_extend_send(self):
-        print('extend send')
+        ''' 扩展发送区的单个发送事件触发
+            只发送被按下发送的数据框数据
+        '''
+        # 因为要循环检测哪个被按下了，所以事件要设置成按下触发
+        # 如果设置成弹起触发，则无法分辨谁被按下了
         for extend in self.extend_send_info:
             if extend['btn'].isDown():
                 data = extend['data'].displayText()
@@ -190,15 +240,24 @@ class Message(QThread):
                 self.send(data, self.cur_encode, 0)
 
     def _event_extend_all_select(self, state):
+        ''' 扩展区的全选事件
+            所有的复选框会与 state 状态同步
+        '''
         for extend in self.extend_send_info:
             extend['select'].setCheckState(state)
 
     def _stop_extend_send(self):
+        ''' 停止扩展区的自动发送
+        '''
         self.auto_send_mode = AUTO_SEND_NONE
         self.auto_send.stop()
         ui.b_extend_send_all.setText('发送按顺序')
 
     def _event_extend_all_send(self):
+        ''' 扩展发送的顺序发送按钮触发，
+            按照顺序将选中的数据一次发出
+            如果勾选了循环发送，则会循环的发送直到手动停止
+        '''
         if self.auto_send_mode == AUTO_SEND_MAIN:
             self._stop_main_auto_send()
         if self.auto_send_mode == AUTO_SEND_EXTEND:
@@ -213,6 +272,7 @@ class Message(QThread):
                 self.auto_send.start()
 
     def _event_send(self):
+        ''' 发送区的数据发送 '''
         if self.cur_connect.status():
             data = ui.e_send.toPlainText()
             if ui.c_hex_send.checkState():
@@ -227,11 +287,14 @@ class Message(QThread):
             debug.err('当前没有连接')
 
     def _event_status_control(self):
+        ''' 根据当期的连接模式，控制连接的打开/关闭 
+        '''
         if self.cur_connect.status():
             self.cur_connect.close()
             ui.b_status_control.setText('未连接')
             self.state = False
         else:
+            # 因为要同步改变显示状态，所以分别调用对应连接的打开比较方便
             if self.connect_mode == CONNTET_SERIAL:
                 self._event_open_serial()
             elif self.connect_mode == CONNECT_TCP_CLINET:
@@ -241,6 +304,10 @@ class Message(QThread):
             self.state = True
 
     def _event_open_serial(self):
+        ''' 点击串口连接事件
+            只有打开没有关闭功能
+            会断开其他连接
+        '''
         if self.connect_mode != CONNTET_SERIAL:
             self.cur_connect.close()
         self.serial.event_open()
@@ -253,6 +320,10 @@ class Message(QThread):
         self.set_connect_mode(CONNTET_SERIAL)
 
     def _event_tcp_client_connect(self):
+        ''' 点击 TCP客户端 连接事件
+            只有打开没有关闭功能
+            会断开其他连接
+        '''
         if self.connect_mode != CONNECT_TCP_CLINET:
             self.cur_connect.close()
         self.client.event_open()
@@ -265,6 +336,10 @@ class Message(QThread):
         self.set_connect_mode(CONNECT_TCP_CLINET)
 
     def _event_tcp_server_connect(self):
+        ''' 点击 TCP服务器 连接事件
+            只有打开没有关闭功能
+            会断开其他连接
+        '''
         if self.connect_mode != CONNECT_TCP_SERVER:
             self.cur_connect.close()
         self.server.event_open()
@@ -277,27 +352,18 @@ class Message(QThread):
         self.set_connect_mode(CONNECT_TCP_SERVER)
 
     def _event_clean_send(self):
+        ''' 清空底部发送区数据 '''
         ui.lcd_send_len.display(0)
         ui.e_send.clear()
 
-    def _event_clean_recv(self):
-        pass
-
-    def _event_hex_send(self):
-        pass
-
-    def _event_file(self):
-        pass
-
-    def _event_set_cache(self):
-        pass
-
     def _stop_main_auto_send(self):
+        ''' 停止底部发送区的自动发送 '''
         self.auto_send.stop()
         ui.c_auto_send.setCheckState(False)
         self.auto_send_mode = AUTO_SEND_NONE
 
     def _event_auto_send(self, state):
+        ''' 开始底部发送区的自动发送事件 '''
         if state:
             if self.auto_send_mode == AUTO_SEND_EXTEND:
                 self._stop_extend_send()
@@ -307,6 +373,10 @@ class Message(QThread):
             self._stop_main_auto_send()
 
     def _event_auto_send_timer(self):
+        ''' 发送区自动发送和扩展区循环发送的定时器处理函数
+            两种模式公用一个发送时间
+            每次只有一个模式发送，在各自的触发函数中处理的
+        '''
         t = ui.e_auto_send_time.value() * 1000  # ms
         if self.auto_send_mode == AUTO_SEND_MAIN:
             self._event_send()

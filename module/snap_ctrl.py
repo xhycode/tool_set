a997551531eff4d57744e041c3c4b6c6274c3443
xhycode <731374786@qq.com>
2021-02-03 00:18:42 +0800

    Feature: 添加3D头控制功能

--- module/snap_ctrl.py
+++ module/snap_ctrl.py
@@ -13,7 +13,10 @@ class SnapControl(QThread, ModuleBase):
         super().__init__()
         ModuleBase.__init__(self)
         self.step_init()
+        self.print3d_init()
+        self.cur_connect = CONNECT_TYPE_PC
 
+    # 运动控制
     def step_init(self):
         ui.x_add.clicked.connect(self.move_x_add)
         ui.x_sub.clicked.connect(self.move_x_sub)
@@ -128,9 +131,61 @@ class SnapControl(QThread, ModuleBase):
     def home_b(self):
         self.home("B")
 
-    def check_connect_type():
+    # 3D头控制
+    def print3d_init(self):
+        ui.set_fan_0.clicked.connect(self.set_print_fan0)
+        ui.set_fan_1.clicked.connect(self.set_print_fan1)
+        ui.set_nozzle_temp.clicked.connect(self.set_nozzle_temp)
+        ui.set_bed_temp.clicked.connect(self.set_bed_temp)
+
+    def set_fan_power(self, index, power):
+        if power > 100:
+            power = 100
+        if power < 0:
+            power = 0
+        cmd = "M106 P{} S{}".format(index, power * 255 // 100)
+        self.send_str(cmd)
+
+    def set_print_fan0(self):
+        power = ui.fan_0_target.value()
+        self.set_fan_power(0, power)
+
+    def set_print_fan1(self):
+        power = ui.fan_1_target.value()
+        self.set_fan_power(1, power)
+
+    def set_nozzle_temp(self):
+        temp = ui.nozzle_temp_target.value()
+        cmd = "M104 S{}".format(temp)
+        self.send_str(cmd)
+
+    def set_bed_temp(self):
+        temp = ui.bed_temp_target.value()
+        cmd = "M109 S{}".format(temp)
+        self.send_str(cmd)
+
+    def show_nozzle_temp(self, cur, target):
+        ui.nozzle_temp_val.setText("{}/{}".format(cur, target))
+
+    def show_bed_temp(self, cur, target):
+        ui.bed_temp_val.setText("{}/{}".format(cur, target))
+
+    def show_cut_switch_status(self, status):
+        if status:
+            ui.cut_switch_status.setText("有")
+        else:
+            ui.cut_switch_status.setText("无")
+
+    def show_probe_status(self, status):
+        if status:
+            ui.probe_status.setText("开")
+        else:
+            ui.probe_status.setText("关")
+
+
+    def check_connect_type(self):
         pass
 
     def run(self):
         while  True:
-            slef.check_connect_type()
+            self.check_connect_type()

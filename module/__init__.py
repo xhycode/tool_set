fc47390ddfad6cc823a8d8526d627770b4f593e1
xhycode <731374786@qq.com>
2021-11-17 18:38:19 +0800

    Feature: 任意模块收到的数据都会再串口界面显示

--- module/__init__.py
+++ module/__init__.py
@@ -7,27 +7,34 @@ from module import snap_ctrl
 from ui import ui
 import cfg
 
+
+MODULE_TYPE_DISPLAY = 0
+MODULE_TYPE_WAVEFORM = 1
+MODULE_TYPE_SNAP_CTRL = 2
 class ModuleManage(module_base.ModuleBase):
     def __init__(self):
         pass
         self._mode_win = ui.mode_manage
         self._mode_win.currentChanged.connect(self._event_chenge)
-        self._module_table = {0: data_display.DataDisplay(),
-                             1: waveform.Waveform(),
-                             2: snap_ctrl.SnapControl()}
+        self._module_table = {MODULE_TYPE_DISPLAY: data_display.DataDisplay(),
+                             MODULE_TYPE_WAVEFORM: waveform.Waveform(),
+                             MODULE_TYPE_SNAP_CTRL: snap_ctrl.SnapControl()}
         self._set_cur_module()
 
     def _set_cur_module(self):
         last_index = int(cfg.get(cfg.LAST_MODULE_INDEX, '0'))
         self._mode_win.setCurrentIndex(last_index)
         self._cur_module = self._module_table[last_index]
+        self.cur_module_index = last_index
 
     def _event_chenge(self):
-        cur_index = self._mode_win.currentIndex()
-        self._cur_module = self._module_table[cur_index]
-        cfg.set(cfg.LAST_MODULE_INDEX, str(cur_index))
+        self.cur_module_index = self._mode_win.currentIndex()
+        self._cur_module = self._module_table[self.cur_module_index]
+        cfg.set(cfg.LAST_MODULE_INDEX, str(self.cur_module_index))
 
     def parse(self, data):
+        if self.cur_module_index != MODULE_TYPE_DISPLAY:
+            self._module_table[MODULE_TYPE_DISPLAY].parse(data)
         self._cur_module.parse(data)
 
     def send_pop(self):

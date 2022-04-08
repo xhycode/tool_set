825ac45e665c8ade3dd4ce1b32dab3acd361c6d6
xhycode <731374786@qq.com>
2022-04-08 15:55:48 +0800

    Feature: 升级版本号V1.5.3

--- ui/global_ui.py
+++ ui/global_ui.py
@@ -11,7 +11,7 @@ from PyQt5.QtCore import Qt
 from ui.main_window import Ui_ToolSet
 import sys
 
-TOOL_VERSIONS = 'V1.5.2'
+TOOL_VERSIONS = 'V1.5.3'
 
 class GloabalUI(QMainWindow, Ui_ToolSet):  # 继承类
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling) 
